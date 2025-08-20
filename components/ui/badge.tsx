fb73e47938ae516f5fdc1cae713727ee6ef45bb7
dikarnnd <dika4kiga@gmail.com>
2025-08-20 11:23:01 +0700

    fix problem import file in ui folder

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/app/lib/utils"
+import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
   "inline-flex items-center font-medium rounded-full whitespace-nowrap shrink-0 transition-colors",
