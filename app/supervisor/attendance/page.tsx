8109f5a701a5f4d21dbae36e458d5e3dc2c233aa
nxtrizz10 <rizcyorlando1003@gmail.com>
2025-08-20 09:52:34 +0700

    Fix Supervisor

--- app/supervisor/attendance/page.tsx
+++ app/supervisor/attendance/page.tsx
@@ -1,3 +1,13 @@
+// import LayoutWrapper from '@/components/layout/LayoutWrapper'
+
+// export default function SupervisorAttendance() {
+//   return (
+//     <LayoutWrapper>
+//       <h1>Attendance Supervisor</h1>
+//     </LayoutWrapper>
+//   )
+// }
+
 // src/components/AttendanceHistory.tsx
 "use client";
 import React, { useEffect, useState } from "react";
