93b4103d48665064e6255532c946ffe75eb07881
dikarnnd <dika4kiga@gmail.com>
2025-08-20 11:16:20 +0700

    fix error layoutwrapper

--- components/layout/LayoutWrapper.tsx
+++ components/layout/LayoutWrapper.tsx
@@ -1,20 +1,16 @@
-// components/layout/LayoutWrapper.tsx
 "use client";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+// import { AppSidebar } from "@/components/layout/AppSidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
 import Navbar from "@/components/layout/Navbar";
 import { FC, ReactNode } from "react";
-import { AppSidebar } from "@/components/app-sidebar"; // Pastikan ini versi client-friendly
-import { SessionData } from '@/lib/server/auth';
 
 interface LayoutWrapperProps {
   children: ReactNode;
-  session: SessionData | null; // Terima session sebagai prop
 }
 
-const LayoutWrapper: FC<LayoutWrapperProps> = ({ 
-  children,
-  sidebar 
-}) => {
+const LayoutWrapper: FC<LayoutWrapperProps> = ({ children }) => {
   return (
     <div className='flex flex-col w-full'>
       <Navbar />
@@ -22,7 +18,6 @@ const LayoutWrapper: FC<LayoutWrapperProps> = ({
         <SidebarProvider>
           <div className='flex min-h-screen w-full'>
             <AppSidebar />
-            {sidebar}
             <SidebarInset>
               <main className='p-4 mt-16 sm:mt-12'>{children}</main>
             </SidebarInset>
