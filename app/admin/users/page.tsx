b993a4a41921dbe5a687c87570fca3d73be2f4c2
dikarnnd <dika4kiga@gmail.com>
2025-08-20 10:44:01 +0700

    add dummy data admin/users

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,9 @@
+import LayoutWrapper from '@/components/layout/LayoutWrapper'
+
+export default function AdminUsers() {
+  return (
+    <LayoutWrapper>
+      <h1>Daftar User</h1>
+    </LayoutWrapper>
+  )
+}
