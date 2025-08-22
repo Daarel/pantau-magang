35a4443cebc70049a73f8b571c01ce731217b4e4
dikarnnd <dika4kiga@gmail.com>
2025-08-22 13:40:54 +0700

    styling dashboard intern

--- app/intern/dashboard/page.tsx
+++ app/intern/dashboard/page.tsx
@@ -5,8 +5,8 @@ import { GoClock } from "react-icons/go";
 import { FiCalendar, } from "react-icons/fi";
 import { IoDocumentTextOutline } from "react-icons/io5";
 // Components
-import { Badge } from "@/components/ui/badge"
 import PieChart from '@/components/PieChart';
+import TodaysAttendance from '@/components/TodaysAttendance';
 // Styles
 import '../../globals.css'
 
@@ -16,42 +16,23 @@ export default function InternDashboard() {
       <div className='flex flex-col items-center min-h-screen gap-4'>
         {/* 1. Name, date, time */}
         <div className='bg-blue-500 w-full p-4 rounded-md'>
-          <h2 className='h2 mb-2 text-white'>Welcome, M Bagas Ferdiansyah!</h2>
-          <h6 className='h6 mb-2 text-white'>Thursday, August 14th, 2025</h6>
-          <h6 className='h6 mb-2 text-white'>13:18:15</h6>
+          <div className='flex flex-col mb-2 xl:flex-row'>
+            <h1 className='h1 text-white pr-0 xl:pr-2'>Selamat Datang,</h1>
+            <h1 className='h1 text-white'>Dika Arnanda Putra!</h1>
+          </div>
+          <h5 className='h5 mb-2 text-white'>Thursday, August 14th, 2025</h5>
+          <h5 className='h5 mb-2 text-white'>13:18:15</h5>
         </div>
 
         {/* 2. Today attendance, Piechart */}
         <div className='flex flex-col md:flex-row w-full gap-3'>
           {/* Attendance */}
-          <div className='flex flex-col w-full md:w-1/2 border-2 gap-6 p-4 rounded-md'>
-            {/* Header */}
-            <div className='flex items-center justify-center gap-3'>
-              <GoClock className='text-blue-500 w-6 h-6' />
-              <h4 className='h5 font-semibold'> Today&apos;s Attendance</h4>
-            </div>
-
-            {/* Status */}
-            <div className='flex flex-col px-5 gap-4'>
-              <div className='flex justify-between'>
-                <h1>Status:</h1>
-                <Badge variant="default" className='bg-[#FEF9C3] text-[#854D0E]'>Badge</Badge>
-              </div>
-              <div className='flex justify-between'>
-                <h1>Check In:</h1>
-                <h1 className='font-semibold'>13:43:15</h1>
-              </div>
-              <div className='flex justify-between'>
-                <h1>Check Out:</h1>
-                <h1 className='font-semibold'>13:43:20</h1>
-              </div>
-            </div>
-          </div>
-
+          <TodaysAttendance />
+          
           {/* Piechart */}
-          <div className='flex flex-col w-full md:w-1/2 border-2 gap-4 p-4 rounded-md'>
+          <div className='flex flex-col w-full md:w-1/2 border-2 gap-4 py-4 px-5 rounded-md'>
             {/* Chart */}
-            <div className='flex items-center justify-center'>
+            <div className='flex items-center justify-center w-full'>
               <PieChart />
             </div>
           </div>
--- components/PieChart.tsx
+++ components/PieChart.tsx
@@ -1,32 +1,32 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, Title, Tooltip, ArcElement } from "chart.js";
+// icons
+import { AiOutlinePieChart } from 'react-icons/ai';
 
 // Registrasi komponen Chart.js
 ChartJS.register(Title, Tooltip, ArcElement);
 
 export default function PieChart() {
   // Data random
-  const labels = ["Present", "Absent", "Late", "early_leave"];
+  const labels = ["Hadir", "Alfa", "Sakit"];
   const dataValues = labels.map(() => Math.floor(Math.random() * 100) + 10); // nilai random 10-110
 
   const data = {
     labels,
     datasets: [
       {
-        label: "Penjualan",
+        label: "Kehadiran",
         data: dataValues,
         backgroundColor: [
           "rgba(255, 99, 132, 0.7)",
           "rgba(54, 162, 235, 0.7)",
           "rgba(255, 206, 86, 0.7)",
-          "rgba(75, 192, 192, 0.7)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 1)",
           "rgba(54, 162, 235, 1)",
           "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
         ],
         borderWidth: 1,
       },
@@ -35,6 +35,7 @@ export default function PieChart() {
 
   const options = {
     responsive: true,
+    maintainAspectRatio: false,
     plugins: {
       // legend: { position: "bottom" },
       // title: { display: true, text: "Pie Chart Contoh 4 Field" },
@@ -42,8 +43,30 @@ export default function PieChart() {
   };
 
   return (
-    <div className='flex items-center justify-center w-2/3 xl:w-1/2'>
-      <Pie data={data} options={options} />
+    <div className='flex flex-col items-center justify-center gap-6'>
+      {/* Judul section */}
+      <div className="flex items-center gap-2">
+        <AiOutlinePieChart className="w-6 h-6 text-blue-500" />
+        <h3 className="h4 font-semibold">Persentase Kehadiran</h3>
+      </div>
+      <div className="flex items-center gap-5 md:gap-10">
+        {/* Legend manual di kiri */}
+        <div className="flex flex-col gap-3">
+          {labels.map((label, idx) => (
+            <div key={idx} className="flex items-center gap-2">
+              <div
+                className="w-4 h-4 rounded"
+                style={{ backgroundColor: data.datasets[0].backgroundColor[idx] }}
+              ></div>
+              <span>{label}</span>
+            </div>
+          ))}
+        </div>
+        <div className="w-full max-w-[180px] md:max-w-[180px] aspect-square">
+          <Pie data={data} options={options} />
+        </div>
+      </div>
+
     </div>
   );
 }
