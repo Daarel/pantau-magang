5f82d71c544744ab38c403ce338bf9f592017a42
nxtrizz10 <rizcyorlando1003@gmail.com>
2025-08-31 13:40:19 +0700

    fix supervisor-tabel

--- app/supervisor/attendance/component/AttendanceClient.tsx
+++ app/supervisor/attendance/component/AttendanceClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-// import TabelSupervisor from "@/app/tabel-supervisor/page";
+import { AttendanceTable } from "@/app/tabel-supervisor/page";
 
 export default function AttendanceClient() {
   const [activeTab, setActiveTab] = useState("Semua Daftar");
@@ -37,7 +37,7 @@ export default function AttendanceClient() {
 
       {/* Content */}
       <div className="mt-6 space-y-4">
-        {/* <TabelSupervisor activeTab={activeTab} type="attendance" serverData={[]} /> */}
+        <AttendanceTable/>
       </div>
     </div>
   );
---
+++ app/tabel-supervisor/page.tsx
@@ -0,0 +1,197 @@
+
+import { columns, reportColumns, Attendance, Report} from "./columns"
+import { DataTable } from "./data-table"
+
+function getData(): Attendance[] {
+  // Fetch data from your API here.
+  return [
+    { 
+      name: "Andi",
+      status: "Hadir",
+      keterangan: "-",
+      date: "2024-06-01", 
+      check_in_time: "08:00", 
+      check_out_time: "08:00",
+    },
+    { 
+      name: "Budi",
+      status: "Hadir", 
+      keterangan: "-",
+      date: "2024-06-01",   
+      check_in_time: "08:05", 
+      check_out_time: "08:05",
+    },
+    { 
+      name: "Citra",
+      status: "Hadir",
+      keterangan: "-",
+      date: "2024-06-01",  
+      check_in_time: "08:30", 
+      check_out_time: "08:30",
+    },
+    { 
+      name: "Dewi",
+      status: "Hadir", 
+      keterangan: "-",
+      date: "2024-06-01", 
+      check_in_time: "08:10", 
+      check_out_time: "08:10",
+    },
+    { 
+      name: "Eka",
+      status: "Hadir", 
+      keterangan: "-",
+      date: "2024-06-01", 
+      check_in_time: "08:15", 
+      check_out_time: "08:15",
+    },
+    { 
+      name: "Fajar",
+      status: "Sakit", 
+      keterangan: "-",
+      date: "2024-06-01", 
+      check_in_time: "-:-", 
+      check_out_time: "-:-" },
+
+      { 
+      name: "Gina",
+      status: "Hadir", 
+      keterangan: "-",
+      date: "2024-06-01", 
+      check_in_time: "08:20", 
+      check_out_time: "08:20",
+    },
+    { 
+      name: "Hari",
+      status: "Hadir",
+      keterangan: "-",
+      date: "2024-06-01",  
+      check_in_time: "08:25", 
+      check_out_time: "08:25",
+    },
+    { 
+      name: "Intan",
+      status: "Alfa", 
+      keterangan: "-",
+      date: "2024-06-01", 
+      check_in_time: "-:-", 
+      check_out_time: "-:-" },
+
+      { 
+      name: "Joko",
+      status: "Izin",
+      keterangan: "-",
+      date: "2024-06-01",  
+      check_in_time: "-:-", 
+      check_out_time: "-:-",
+    },
+
+    { 
+      name: "Ucok",
+      status: "Alfa", 
+      keterangan: "-",
+      date: "2024-06-04", 
+      check_in_time: "-:-", 
+      check_out_time: "-:-" },
+
+    { 
+      name: "Abeng",
+      status: "Sakit", 
+      keterangan: "-",
+      date: "2024-06-05", 
+      check_in_time: "-:-", 
+      check_out_time: "-:-" },
+
+  ]
+}
+
+function getReportData(): Report[] {
+  // Fetch data from your API here.
+  return [
+    { 
+      file: "report_andi.pdf",
+      name: "Andi",
+      status: "Izin",
+      keterangan: "Izin ke kampus",
+    },
+
+    { 
+      file: "report_sandi.pdf",
+      name: "Sandi",
+      status: "Sakit",
+      keterangan: "Izin Sakit",
+    },
+
+    { 
+      file: "report_andi.pdf",
+      name: "Andi",
+      status: "Sakit",
+      keterangan: "Masih Sakit",
+    },
+
+    { 
+      file: "report_sandi.pdf",
+      name: "Sandi",
+      status: "Sakit",
+      keterangan: "Ketularan Sakit",
+    },
+
+    { 
+      file: "report_dono.pdf",
+      name: "Dono",
+      status: "Izin",
+      keterangan: "Main ke luar kota",
+    },
+
+    { 
+      file: "report_dono.pdf",
+      name: "Dono",
+      status: "Sakit",
+      keterangan: "Demam tinggi",
+    },
+
+    { 
+      file: "report_sandi.pdf",
+      name: "Sandi",
+      status: "Izin",
+      keterangan: "Tidak ada keterangan",
+    },
+
+    { 
+      file: "report_vior.pdf",
+      name: "Vior",
+      status: "Izin",
+      keterangan: "Nikah",
+    },
+  ]
+
+}
+
+export function AttendanceTable() {
+  const attendanceData = getData()
+  return (
+    <div className="w-full">
+      <DataTable
+        columns={columns}
+        data={attendanceData}
+        enableFilter={false}
+        enableColumnVisibility={false}
+      />
+    </div>
+  )
+}
+
+// Komponen khusus Report
+export function ReportTable() {
+  const reportData = getReportData()
+  return (
+    <div className="w-full">
+      <DataTable
+        columns={reportColumns}
+        data={reportData}
+        enableFilter={false}
+        enableColumnVisibility={false}
+      />
+    </div>
+  )
+}
