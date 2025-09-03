4347eb36a5dccdb5009658cce46aa3f91353e1f3
Fatillah <daarel.fatillah@outlook.com>
2025-09-03 14:56:38 +0700

    making validation for input modal

--- app/admin/supervisor/page.tsx
+++ app/admin/supervisor/page.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import AdminSupervisorClient from './components/AdminSupervisorClient';
+import Loading from "../loading";
 
 export default function AdminUserPage() {
   // jika ingin fetch data dari DB: make this async and fetch here, lalu pass data ke client
   return (
-    <Suspense fallback={<div className="flex justify-center items-center">Tunggu ya...</div>}>
+    <Suspense fallback={<Loading />}>
       <div className='min-h-screen bg-gray-50 p-6'>
         <AdminSupervisorClient />
       </div>
