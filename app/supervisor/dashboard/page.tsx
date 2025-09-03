0bed598973424b6bb60096f0da3f0c39dc1dbae9
nxtrizz10 <rizcyorlando1003@gmail.com>
2025-09-03 12:33:55 +0700

    Card Content Style

--- app/supervisor/dashboard/page.tsx
+++ app/supervisor/dashboard/page.tsx
@@ -6,6 +6,9 @@ import TodayInternStatus from '@/components/today-intern-status/page';
 import { auth } from '@/lib/server/auth'; 
 import '../../globals.css';
 import Image from 'next/image';
+import DashboardClock from "@/components/DashboardClock";
+import { Card, CardContent } from "@/components/Card";
+import StatCard from "@/components/StatCard";
 
 export default async function SupervisorDashboard() {
   const session = await auth();
@@ -25,63 +28,52 @@ export default async function SupervisorDashboard() {
           <h1 className='title_header max-sm:text-3xl'>
             Selamat Datang, Dika Arnanda Putra!
           </h1>
-          <h2 className='text-white text-2xl max-sm:text-lg'>
-            Kamis, 14 Agustus 2025
-          </h2>
-          <p className='text-white text-lg max-sm:text-sm'>13:18:15</p>
+          <DashboardClock/>
         </div>
       </div>
 
-      {/* 2. Intern Overview */}
-      <div className='flex flex-col w-full gap-4 sm:flex-row'>
-        {/* Total interns & present today */}
-        <div className='flex w-full sm:w-1/2 gap-2 sm:gap-4'>
-          {/* Total interns */}
-          <div className='flex w-full sm:w-1/2 p-4 items-center justify-center sm:justify-evenly border-2 rounded-md gap-4'>
-            <div>
-              <GoPeople className='text-blue-600 bg-blue-200 p-3 w-11 lg:w-13 h-11 lg:h-13 rounded-md' />
-            </div>
-            <div className='flex flex-col gap-1'>
-              <h6 className='h6 font-semibold text-black/50'>Total Interns</h6>
-              <h1 className='h3 font-bold'>4</h1>
-            </div>
-          </div>
-
-            {/* Present today */}
-            <div className='flex w-full sm:w-1/2 p-4 items-center justify-center sm:justify-evenly border-2 rounded-md gap-4'>
-              <div>
-                <GoClock className='text-green-600 bg-green-200 p-3 w-11 lg:w-13 h-11 lg:h-13 rounded-md' />
-              </div>
-              <div className='flex flex-col gap-1'>
-                <h6 className='h6 font-semibold text-black/50'>Present Today</h6>
-                <h1 className='h3 font-bold'>4</h1>
-              </div>
-            </div>
-          </div>
-
-          {/* Pending Leaves & Avg Attendance */}
-          <div className='flex w-full sm:w-1/2 gap-2 sm:gap-4'>
-            <div className='flex w-1/2 p-4 items-center justify-center sm:justify-evenly border-2 rounded-md gap-4'>
-              <div>
-                <IoDocumentTextOutline className='text-[#CA8A04] bg-[#FEF9C3] p-3 w-11 lg:w-13 h-11 lg:h-13 rounded-md' />
-              </div>
-              <div className='flex flex-col gap-1'>
-                <h1 className='h6 font-semibold text-black/50'>Pending Leaves</h1>
-                <h1 className='h3 font-bold'>2</h1>
-              </div>
-            </div>
-
-            <div className='flex w-1/2 p-4 border-2 items-center justify-center sm:justify-evenly rounded-md gap-4'>
-              <div>
-                <FiTrendingUp className='text-purple-600 bg-purple-200 p-3 w-11 lg:w-13 h-11 lg:h-13 rounded-md' />
-              </div>
-              <div className='flex flex-col gap-1'>
-                <h1 className='h6 font-semibold text-black/50'>Avg Attendance</h1>
-                <h1 className='h3 font-bold'>1</h1>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className='grid grid-cols-4 max-sm:grid-cols-2 gap-6 max-md:grid-cols-2 mb-5'>
+        <Card>
+          <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
+            <StatCard
+              Icon={GoPeople}
+              title='Total Interns'
+              value="4"
+              contentColor='text-blue-600'
+            />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
+            <StatCard
+              Icon={GoClock}
+              title='Present Today'
+              value={"4"}
+              contentColor='text-green-600'
+            />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
+            <StatCard
+              Icon={IoDocumentTextOutline}
+              title='Pending Leaves'
+              value={"2"}
+              contentColor='text-yellow-600'
+            />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
+            <StatCard
+              Icon={FiTrendingUp}
+              title='Avg Attendance'
+              value={"100"}
+              contentColor='text-indigo-600'
+            />
+          </CardContent>
+        </Card>
+      </div>
 
       {/* Today's Intern Status */}
       <TodayInternStatus />
