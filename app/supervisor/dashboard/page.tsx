413150692ef2ca1d176e6e6eb90d7f232702d417
nxtrizz10 <rizcyorlando1003@gmail.com>
2025-09-03 14:43:31 +0700

    change table intern to supervisor

--- app/supervisor/dashboard/page.tsx
+++ app/supervisor/dashboard/page.tsx
@@ -1,17 +1,51 @@
 import { GoClock, GoPeople } from "react-icons/go";
 import { FiTrendingUp } from "react-icons/fi";
 import { IoDocumentTextOutline } from "react-icons/io5";
-
-import TodayInternStatus from '@/components/today-intern-status/page';
 import { auth } from '@/lib/server/auth'; 
 import '../../globals.css';
 import Image from 'next/image';
 import DashboardClock from "@/components/DashboardClock";
 import { Card, CardContent } from "@/components/Card";
 import StatCard from "@/components/StatCard";
+import { DashboardTable } from "@/components/tabel-supervisor/AttendanceTable";
 
 export default async function SupervisorDashboard() {
   const session = await auth();
+
+  const stats = {
+    totalInterns: 4,
+    presentToday: 2,
+    pendingLeaves: 50,
+    avgAttendance: 2,
+  };
+
+  const statCards = [
+      {
+        Icon: GoPeople,
+        title: "Total Interns",
+        value: stats.totalInterns,
+        contentColor: "text-blue-600",
+      },
+      {
+        Icon: GoClock,
+        title: "Present Today",
+        value: stats.presentToday,
+        contentColor: "text-green-600",
+      },
+      {
+        Icon: IoDocumentTextOutline,
+        title: "Pending Leaves",
+        value: stats.pendingLeaves,
+        contentColor: "text-yellow-600",
+      },
+      {
+        Icon: FiTrendingUp,
+        title: "Avg Attendance",
+        value: stats.avgAttendance,
+        contentColor: "text-indigo-600",
+      },
+    ];
+
 
   return (
     <> 
@@ -33,50 +67,22 @@ export default async function SupervisorDashboard() {
       </div>
 
       <div className='grid grid-cols-4 max-sm:grid-cols-2 gap-6 max-md:grid-cols-2 mb-5'>
-        <Card>
-          <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
-            <StatCard
-              Icon={GoPeople}
-              title='Total Interns'
-              value="4"
-              contentColor='text-blue-600'
-            />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
-            <StatCard
-              Icon={GoClock}
-              title='Present Today'
-              value={"4"}
-              contentColor='text-green-600'
-            />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
-            <StatCard
-              Icon={IoDocumentTextOutline}
-              title='Pending Leaves'
-              value={"2"}
-              contentColor='text-yellow-600'
-            />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
-            <StatCard
-              Icon={FiTrendingUp}
-              title='Avg Attendance'
-              value={"100"}
-              contentColor='text-indigo-600'
-            />
-          </CardContent>
-        </Card>
+        {statCards.map((card, i) => (
+          <Card key={i}>
+            <CardContent className='flex items-center p-3 max-lg:p-0 max-lg:flex-col max-lg:gap-1'>
+              <StatCard
+                Icon={card.Icon}
+                title={card.title}
+                value={card.value}
+                contentColor={card.contentColor}
+              />
+            </CardContent>
+          </Card>
+        ))}
       </div>
 
       {/* Today's Intern Status */}
-      <TodayInternStatus />
+      <DashboardTable />
     </>
   );
 }
--- components/tabel-supervisor/AttendanceTable.tsx
+++ components/tabel-supervisor/AttendanceTable.tsx
@@ -1,5 +1,5 @@
 
-import { columns, reportColumns, Attendance, Report} from "./columns"
+import { columns, reportColumns, Dashboardcolumns, Attendance, Report, Dashboard} from "./columns"
 import { DataTable } from "./data-table"
 
 function getData(): Attendance[] {
@@ -167,6 +167,97 @@ function getReportData(): Report[] {
 
 }
 
+function getDashboardData(): Dashboard[] {
+  // Fetch data from your API here.
+  return [
+    { 
+      name: "Andi",
+      status: "Hadir",
+      institutions: "UPJ", 
+      check_in_time: "08:00", 
+      check_out_time: "08:00",
+    },
+    { 
+      name: "Budi",
+      status: "Hadir", 
+      institutions: "UPJ", 
+      check_in_time: "08:05", 
+      check_out_time: "08:05",
+    },
+    { 
+      name: "Citra",
+      status: "Hadir",
+      institutions: "UPJ",  
+      check_in_time: "08:30", 
+      check_out_time: "08:30",
+    },
+    { 
+      name: "Dewi",
+      status: "Hadir", 
+      institutions: "UPJ", 
+      check_in_time: "08:10", 
+      check_out_time: "08:10",
+    },
+    { 
+      name: "Eka",
+      status: "Hadir", 
+      institutions: "UPJ", 
+      check_in_time: "08:15", 
+      check_out_time: "08:15",
+    },
+    { 
+      name: "Fajar",
+      status: "Sakit", 
+      institutions: "UPH",
+      check_in_time: "-:-", 
+      check_out_time: "-:-" },
+
+      { 
+      name: "Gina",
+      status: "Hadir", 
+      institutions: "UNPAM", 
+      check_in_time: "08:20", 
+      check_out_time: "08:20",
+    },
+    { 
+      name: "Hari",
+      status: "Hadir",
+      institutions: "UNTIRTA",  
+      check_in_time: "08:25", 
+      check_out_time: "08:25",
+    },
+    { 
+      name: "Intan",
+      status: "Alfa", 
+      institutions: "UPH", 
+      check_in_time: "-:-", 
+      check_out_time: "-:-" },
+
+      { 
+      name: "Joko",
+      status: "Izin",
+      institutions: "UNPAM",  
+      check_in_time: "-:-", 
+      check_out_time: "-:-",
+    },
+
+    { 
+      name: "Ucok",
+      status: "Alfa", 
+      institutions: "UBL", 
+      check_in_time: "-:-", 
+      check_out_time: "-:-" },
+
+    { 
+      name: "Abeng",
+      status: "Sakit", 
+      institutions: "UBL", 
+      check_in_time: "-:-", 
+      check_out_time: "-:-" },
+
+  ]
+}
+
 export function AttendanceTable({ activeTab }: { activeTab: string }) {
   const attendanceData = getData()
 
@@ -203,6 +294,21 @@ export function ReportTable({ activeTab }: { activeTab: string }) {
       <DataTable
         columns={reportColumns}
         data={filteredData}
+        enableFilter={false}
+        enableColumnVisibility={false}
+      />
+    </div>
+  )
+}
+
+export function DashboardTable() {
+  const reportData = getDashboardData()
+
+  return (
+    <div className="w-full">
+      <DataTable
+        columns={Dashboardcolumns}
+        data={reportData}
         enableFilter={false}
         enableColumnVisibility={false}
       />
