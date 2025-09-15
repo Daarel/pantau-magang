8c57d893a291c12d7545e546ab7291f936360bcb
Fatillah <daarel.fatillah@outlook.com>
2025-09-15 09:28:43 +0700

    fix production bug

--- components/tabel-supervisor/columns.tsx
+++ components/tabel-supervisor/columns.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "@/components/data-table-column-header";
 import {
