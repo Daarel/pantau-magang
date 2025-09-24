ff244b2ca8021e30e44807af22aa96704f8202f0
Fatillah <daarel.fatillah@outlook.com>
2025-09-24 11:20:05 +0700

    fix style issues on reset page

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 const LoginButton: FC = () => {
   const { pending } = useFormStatus();
   return (
-    <Button type='submit' className='w-full mt-5' disabled={pending}>
+    <Button type='submit' className='w-full text-[#fcf400] bg-foreground mt-7' disabled={pending}>
       {pending ? "Loading..." : "Log in"}
     </Button>
   );
