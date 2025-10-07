26360be0d750e739ffc02c4443d9cb7645ca563a
Fatillah <daarel.fatillah@outlook.com>
2025-10-07 12:57:57 +0700

    fix hooks bug

--- components/AttendanceButtonHandler.tsx
+++ components/AttendanceButtonHandler.tsx
@@ -1,27 +1,32 @@
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { FC } from "react";
 
-export const CheckInButton = (text: string) => {
+interface CheckInButton {
+  text: string;
+}
+
+export const CheckInButton: FC<CheckInButton> = ({ text }) => {
   const router = useRouter();
   return (
-    <Button 
-      size='icon' 
+    <Button
+      size='icon'
       className='w-full bg-green-200 hover:bg-green-300 border-green-700 active:bg-green-400 transition-colors duration-100 shadow'
-      onClick={() => router.push('/intern/attendance')}
+      onClick={() => router.push("/intern/attendance")}
     >
       <h5 className='text-black/80 font-bold'>{text}</h5>
     </Button>
-  )
+  );
+};
+
+interface DisabledButton {
+  text: string;
 }
 
-export const DisabledButton = (text: string) => {
+export const DisabledButton: FC<DisabledButton> = ({ text }) => {
   return (
-    <Button 
-      disabled
-      size='icon' 
-      className='w-full bg-gray-200 shadow'
-    >
+    <Button disabled size='icon' className='w-full bg-gray-200 shadow'>
       <h5 className='text-black/90 font-bold'>{text}</h5>
     </Button>
-  )
-}
+  );
+};
--- components/TodaysAttendance.tsx
+++ components/TodaysAttendance.tsx
@@ -3,23 +3,30 @@ import React from "react";
 import { GoClock } from "react-icons/go";
 import { Badge } from "@/components/ui/badge";
 // Components
-import { CheckOutModal } from "@/components/CheckOutModal"
-import { useDashboardData } from '@/hooks/useDashboardData'
-import { statusColor } from '@/lib/utils';
+import { CheckOutModal } from "@/components/CheckOutModal";
+import { useDashboardData } from "@/hooks/useDashboardData";
+import { statusColor } from "@/lib/utils";
 import { CheckInButton, DisabledButton } from "./AttendanceButtonHandler";
 
 export default function TodaysAttendance() {
-  const { summaryData, loading, error } = useDashboardData()
+  const { summaryData, loading, error } = useDashboardData();
 
-  const status = loading ? "-" : summaryData?.status ?? "-"
-  const todayStatus = statusColor(status)
+  const status = loading ? "-" : summaryData?.status ?? "-";
+  const todayStatus = statusColor(status);
   const isAlfa = todayStatus.text === "Alfa";
   const isIzin = todayStatus.text === "Izin";
   const isSakit = todayStatus.text === "Sakit";
   const isIzinOrSakit = isIzin || isSakit;
 
-  const showCheckInButton = !loading && (!summaryData?.status || summaryData.status === "-" || todayStatus.text === "Belum Tercatat");
-  const hasCheckOut = !loading && summaryData?.today_check_out && summaryData.today_check_out !== "-";
+  const showCheckInButton =
+    !loading &&
+    (!summaryData?.status ||
+      summaryData.status === "-" ||
+      todayStatus.text === "Belum Tercatat");
+  const hasCheckOut =
+    !loading &&
+    summaryData?.today_check_out &&
+    summaryData.today_check_out !== "-";
 
   return (
     <div className='flex flex-col w-full md:w-1/2 border-2 gap-6 py-4 px-5 rounded-md'>
@@ -33,47 +40,51 @@ export default function TodaysAttendance() {
       <div className='flex flex-col gap-4'>
         <div className='flex justify-between'>
           <h1>Status:</h1>
-          <Badge variant="default" className={loading ? "bg-gray-100" : todayStatus.class}>{loading ? "-" : todayStatus.text}</Badge>
+          <Badge
+            variant='default'
+            className={loading ? "bg-gray-100" : todayStatus.class}
+          >
+            {loading ? "-" : todayStatus.text}
+          </Badge>
         </div>
         <div className='flex justify-between'>
           <h1>Check In:</h1>
-          <h1 className='font-semibold'>{loading ? "-" : summaryData?.today_check_in ?? "-"}</h1>
+          <h1 className='font-semibold'>
+            {loading ? "-" : summaryData?.today_check_in ?? "-"}
+          </h1>
         </div>
         <div className='flex justify-between'>
           <h1>Check Out:</h1>
-          <h1 className='font-semibold'>{loading ? "-" : summaryData?.today_check_out ?? "-"}</h1>
+          <h1 className='font-semibold'>
+            {loading ? "-" : summaryData?.today_check_out ?? "-"}
+          </h1>
         </div>
       </div>
 
       {loading ? (
-        DisabledButton("Loading...")
+        <DisabledButton text='Loading...' />
       ) : (
         <>
           {isAlfa && (
-            DisabledButton("yahahahhaha gabisa absen awokaowkawok😂😂😂")
+            <DisabledButton text='yahahahhaha gabisa absen awokaowkawok😂😂😂' />
           )}
 
-          {isIzin && (
-            DisabledButton("Besok harus berangkat!!")
-          )}
+          {isIzin && <DisabledButton text='Besok harus berangkat!!' />}
 
-          {isSakit && (
-            DisabledButton("Semoga lekas sembuh yaa!!😁👍")
-          )}
+          {isSakit && <DisabledButton text='Semoga lekas sembuh yaa!!😁👍' />}
 
           {!isAlfa && showCheckInButton && (
-            CheckInButton("Silakan Absen Masuk")
+            <CheckInButton text='Silakan Absen Masuk' />
           )}
 
           {/* Tidak menampilkan Button Checkout ketika Status Alfa, Izin, Sakit, dan setelah melakukan Checkout */}
-          {!isAlfa && !isIzinOrSakit && !showCheckInButton && !hasCheckOut && <CheckOutModal />}
-
-          {hasCheckOut && (
-            DisabledButton("Besok masuk lagi yaa!")
+          {!isAlfa && !isIzinOrSakit && !showCheckInButton && !hasCheckOut && (
+            <CheckOutModal />
           )}
+
+          {hasCheckOut && <DisabledButton text='Besok masuk lagi yaa!' />}
         </>
       )}
-
     </div>
   );
 }
