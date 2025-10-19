e376597df38ae1be76e67105cd10922da1b17e06
Fatillah <daarel.fatillah@outlook.com>
2025-10-19 22:58:22 +0700

    fix skeleton for root loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,13 +2,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className='flex flex-col gap-4'>
-      <div className='flex flex-row'>
-        <Skeleton className='w-screen h-[200px]' />
-      </div>
-      <div className='flex flex-row'>
-        <Skeleton className='h-full w-[255px]' />
-      </div>
+    <div className='flex flex-col min-h-screen'>
+      <Skeleton className='w-screen h-[80px]' />
+      <Skeleton className='w-[200px] flex-1 rounded-none' />
     </div>
   );
 }
