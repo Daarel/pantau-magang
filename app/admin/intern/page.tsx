7bc890159a35d4a9e0dbe7580fed67780e511d52
Fatillah <daarel.fatillah@outlook.com>
2025-11-06 14:05:37 +0700

    fixing token bug, minor styling bug, and format table row

--- app/admin/intern/page.tsx
+++ app/admin/intern/page.tsx
@@ -7,7 +7,7 @@ import { createClient } from "@/lib/supabase/server";
 import { formatDateID } from "@/lib/helper/formatDate.helper";
 import { toast } from "sonner";
 
-export const revalidate = 60
+export const revalidate = 60;
 
 export default async function AdminUserPage({
   searchParams,
@@ -65,7 +65,7 @@ export default async function AdminUserPage({
 
   return (
     <Suspense fallback={<Loading />}>
-      <div className='min-h-screen bg-gray-50 p-6 overflow-x-hidden'>
+      <div className='w-[calc(100vw-16rem)] h-screen overflow-y-auto bg-gray-50 pr-20 p-6'>
         <AdminInternClient
           tableData={flatData ?? []}
           totalCount={count ?? 0}
