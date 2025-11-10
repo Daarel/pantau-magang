1b250df4365df98f65d36cbc71065be60b4a4796
Fatillah <daarel.fatillah@outlook.com>
2025-11-10 13:13:26 +0700

    upload to vercel

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { createServerClient } from "@supabase/ssr";
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const pathname = request.nextUrl.pathname;
-
+ 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
